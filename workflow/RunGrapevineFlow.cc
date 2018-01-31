80225a2dcf918b8c4e16613acb800046570aa104
grabherr <manfred.grabherr@imbim.uu.se>
2018-01-31 14:26:51 +0100

    Testing, testing, testing...

--- workflow/RunGrapevineFlow.cc
+++ workflow/RunGrapevineFlow.cc
@@ -8,7 +8,7 @@
 
 
 string exec(const char* cmd) {
-  cout << "EXECUTING: " << cmd << endl;
+  //cout << "EXECUTING: " << cmd << endl;
   char buffer[2048];
   string result = "";
   FILE* pipe = popen(cmd, "r");
@@ -36,18 +36,27 @@ string SubmitOne(const string & cmd)
     return "-1";
   }
   string procID = p.AsString(3);
+  if (procID[procID.size()-1] == '\n')
+    procID[procID.size()-1] = 0;
   cout << "Submitted " << cmd << "Process ID: " << procID << endl;
   return procID;
 }
 
 bool CheckExit(string & ret, string & succ, const string & id)
 {
-  string cmd = "sacct --jobs " + id;
-  
+  string cmd = "sacct --jobs " + id /* + " | grep " + id + " |grep -v \\.ba"*/;
+  //cout << "Checking: " << cmd << endl;
   string r = exec(cmd.c_str());
   StringParser p;
-  p.SetLine(ret);
-  cout << ret << endl;
+  p.SetLine(r);
+  //cout << ret << endl;
+  if (p.GetItemCount() < 21) {
+    ret = "<unknown>";
+    succ = "<unknown>";
+  } else {
+    ret = p.AsString(20);
+    succ = p.AsString(19);
+  }
   return true;
 }
 
@@ -80,32 +89,36 @@ int main( int argc, char** argv )
       if (parser.GetItemCount() == 0)
 	continue;
     
-      string id = SubmitOne(parser.Line());
+      string id = "11794110";
+      id = SubmitOne(parser.Line());
       ids.push_back(id);
       status.push_back("");
     }
       
     int counter = ids.isize();
+
     while (counter > 0) {
 
       for (i=0; i<ids.isize(); i++) {
 	if (ids[i] == "")
 	  continue;
 	
-	string cmd = "squeue -j " + ids[i];
-     
+	string cmd = "squeue -j " + ids[i] /*+ " | grep " + ids[i]*/;
+	//cout << cmd << endl;
 	string ret = exec(cmd.c_str());
-	if (ret == "") {	  
+	//cout << "RETURNED " << endl << ret << endl;
+	StringParser p;
+	p.SetLine(ret);
+	if (p.GetItemCount() < 9) {	  
 	  counter--;
 	  string ret1, stat;
 	  CheckExit(ret1, stat, ids[i]);
 	  cout << "Process " << ids[i] << " finished with ret " << ret1 << " status " << stat << endl;
 	  ids[i] = "";
+	  continue;
 	}
-	StringParser p;
-	p.SetLine(ret);
-	string s = p.AsString(4);
-	string t = p.AsString(5);
+	string s = p.AsString(12);
+	string t = p.AsString(13);
 	if (s != status[i]) {
 	  status[i] = s;
 	  cout << "Program " << ids[i] << " has status " << s << endl;
