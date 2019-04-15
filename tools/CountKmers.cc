a88f6d5e76a0439c07290bd77cd9cec35e406c69
grabherr <manfred.grabherr@imbim.uu.se>
2019-04-15 14:59:38 +0200

    Added file.

--- tools/CountKmers.cc
+++ tools/CountKmers.cc
@@ -17,7 +17,38 @@
 #include "src/SequenceMatch.h"
 
 
+class Kmer
+{
+public:
+  Kmer() {
+    count = 0;
+  }
 
+  
+  bool Plus() {
+    int n = count;
+
+    for (int i=dna.isize()-1; i>=0; i--) {
+      int x = n & 0x3;
+      if (x == 0)
+	dna[i] = 'A';
+      if (x == 1)
+	dna[i] = 'C';
+      if (x == 2)
+	dna[i] = 'G';
+      if (x == 3)
+	dna[i] = 'T';
+      n = n >> 2;
+    }
+    
+    count++;
+    return true;
+  }
+  
+  DNAVector dna;
+  int count;
+  
+};
 
 
 
@@ -58,17 +89,31 @@ int main(int argc,char** argv)
 
     for (i=0; i<d.isize()-k; i++) {
       int n = trans.BasesToNumber(d, i);
-      freq[n] += 1.;
+      if (n >= 0)       
+	freq[n] += 1.;
       
     }
     n++;
 
-    if (n > 20)
-      break;
+    // if (n > 20)
+    //break;
   }
-
+  
+  Kmer kmer;
+  kmer.dna.resize(k);
+  kmer.Plus();
+  
+  //cout << kmer.dna.isize() << endl;
   for (i=0; i<freq.isize(); i++) {
-    cout << freq[i] << endl;
+    for (int x=0; x<kmer.dna.isize(); x++)
+      cout << kmer.dna[x];
+    //cout << " Printed" << endl;
+
+    
+    int n = trans.BasesToNumber(kmer.dna, 0);
+    cout << " ";
+    cout << freq[n] << endl;
+    kmer.Plus();
   }
   
 
