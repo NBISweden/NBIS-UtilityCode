815e2e7b846ac3e5b7977a6719bed9a1f606aa50
grabherr <manfred.grabherr@imbim.uu.se>
2019-12-04 09:20:57 +0100

    Make tool generic.

--- tools/FindStrainKmers.cc
+++ tools/FindStrainKmers.cc
@@ -52,12 +52,14 @@ private:
 int main(int argc,char** argv)
 {
 
-  /*commandArg<string> aStringCmmd("-i","fasta file");
+  commandArg<string> iStringCmmd("-i","fasta files (sequence unique to those)");
+  commandArg<string> aStringCmmd("-a","outgroup (sequence no present in these)");
   commandArg<string> oStringCmmd("-o","fasta output");
   commandArg<int> kCmmd("-k","kmer size", 12);
  
   commandLineParser P(argc,argv);
   P.SetDescription("Eliminates redundant sequences.");
+  P.registerArg(iStringCmmd);
   P.registerArg(aStringCmmd);
   P.registerArg(oStringCmmd);
   P.registerArg(kCmmd);
@@ -65,44 +67,53 @@ int main(int argc,char** argv)
 
   P.parse();
 
+  string iString = P.GetStringValueFor(iStringCmmd);
   string aString = P.GetStringValueFor(aStringCmmd);
   string oString = P.GetStringValueFor(oStringCmmd);
-  int k = P.GetIntValueFor(kCmmd);*/
+  int k = P.GetIntValueFor(kCmmd);
 
-  int k = 12;
+  
  
   int i, j;
 
-  vecDNAVector lo1, lo2, lo3;
-  vecDNAVector hi1, hi2, hi3;
+  svec<vecDNAVector> in, ref;
+
+  StringParser ip;
+  ip.SetLine(iString, ",");
+  StringParser ap;
+  ap.SetLine(aString, ",");
+
+  in.resize(ip.GetItemCount());
+  ref.resize(ap.GetItemCount());
   
-  lo1.Read("SMI416_hgap.fasta");
-  lo2.Read("SMI798_hgap.fasta");
-  lo3.Read("SMI706_hgap.fasta");
-
-  hi1.Read("SMI588_hgap.fasta");
-  hi2.Read("SMI596_hgap.fasta");
-  hi3.Read("SMI828_hgap.fasta");
+  for (i=0; i<ip.GetItemCount(); i++)
+    in[i].Read(ip.AsString(i));
+  
+  for (i=0; i<ap.GetItemCount(); i++) {
+    ref[i].Read(ap.AsString(i));
+  }
+  
+ 
+  vecDNAVector out;
 
   
-  vecDNAVector out_lo, out_hi;
-
-  
 
 
-  KmerAlignCore<KmerAlignCoreRecordWithScore> low;
-  KmerAlignCore<KmerAlignCoreRecordWithScore> high;
+  KmerAlignCore<KmerAlignCoreRecordWithScore> core;
+  KmerAlignCore<KmerAlignCoreRecordWithScore> self;
+ 
   
   int num12 = 4;
-  low.SetNumTables(num12);
-  high.SetNumTables(num12);
-  
-  TranslateBasesToNumberExact trans_lo;
-  trans_lo.SetSize(k); // Use 15-mers
-  TranslateBasesToNumberExact trans_hi;
-  trans_hi.SetSize(k); // Use 15-mers
-  low.SetTranslator(&trans_lo);
-  high.SetTranslator(&trans_hi);
+  core.SetNumTables(num12);
+  self.SetNumTables(num12);
+  TranslateBasesToNumberExact trans_c;
+  trans_c.SetSize(k); // Use 15-mers
+  core.SetTranslator(&trans_c);
+
+  TranslateBasesToNumberExact trans_s;
+  trans_s.SetSize(k); // Use 15-mers
+  self.SetTranslator(&trans_s);
+
   
   cout << "Adding targets" << endl;
   /* low.AddData(lo1, false, 1);
@@ -112,56 +123,53 @@ int main(int argc,char** argv)
   high.AddData(hi2, false, 1);
   high.AddData(hi3, false, 1);
   */
-  
-  low.AddData(hi1, false, 1);
-  low.AddData(hi2, false, 1);
-  low.AddData(hi3, false, 1);
-  high.AddData(lo1, false, 1);
-  high.AddData(lo2, false, 1);
-  high.AddData(lo3, false, 1);
- 
-  low.SortAll();
-  high.SortAll();
+
+  for (i=0; i<ref.isize(); i++)
+    core.AddData(ref[i], false, 1);
+  for (i=0; i<in.isize(); i++)
+    self.AddData(in[i], false, 1);
+
+  core.SortAll();
   cout << "done" << endl;
   int nn = 0;
-  for (i=0; i<lo1.lsize(); i++) {
+  for (i=0; i<in[0].lsize(); i++) {
  
 
-    DNAVector d = lo1[i];
-    DNAVector r = d;
-    r.ReverseComplement();
+    const DNAVector & d = (in[0])[i];
 
     int nF = 0;
     int nR = 0;
 
-    cout << "Test seq " << lo1.Name(i) << endl;
+    cout << "Test seq " << in[0].Name(i) << endl;
     int last = -1;
     for (j=0; j<=d.lsize()-k*num12; j++) {
       DNAVector subF, subR;
       subF.SetToSubOf(d, j, k*num12);
-      subR.SetToSubOf(r, j, k*num12);
+      subR.SetToSubOf(d, j, k*num12);
+      subR.ReverseComplement();
+      
       svec<KmerAlignCoreRecordWithScore> matchesF_lo;
       svec<KmerAlignCoreRecordWithScore> matchesR_lo;
       svec<KmerAlignCoreRecordWithScore> matchesF_hi;
       svec<KmerAlignCoreRecordWithScore> matchesR_hi;
 
-      low.GetMatches(matchesF_lo, subF);
-      low.GetMatches(matchesR_lo, subR);
+      self.GetMatches(matchesF_lo, subF);
+      self.GetMatches(matchesR_lo, subR);
       
-      high.GetMatches(matchesF_hi, subF);
-      high.GetMatches(matchesR_hi, subR);
+      core.GetMatches(matchesF_hi, subF);
+      core.GetMatches(matchesR_hi, subR);
 
-      if (matchesF_lo.isize() + matchesR_lo.isize() == 3 &&
+      if (matchesF_lo.isize() + matchesR_lo.isize() == in.isize() &&
 	  matchesF_hi.isize() + matchesR_hi.isize() == 0) {
 
 	if (j == last+1) {
-	  DNAVector & dd = out_lo[out_lo.isize()-1];
+	  DNAVector & dd = out[out.isize()-1];
 	  dd.resize(dd.isize()+1);
 	  dd[dd.isize()-1] = subF[subF.isize()-1];
 	} else {
 	  nn++;
-	  string name = ">kmer_" + Stringify(nn) + "_" + Stringify(out_lo.isize());
-	  out_lo.push_back(subF, name);
+	  string name = ">kmer_" + Stringify(nn) + "_" + Stringify(out.isize());
+	  out.push_back(subF, name);
 	}
   	last = j;
 
@@ -172,66 +180,9 @@ int main(int argc,char** argv)
     
      
   }
-  cout << "Found: " << out_lo.isize() << endl;
-  out_lo.Write("low_exclusive.fasta");
+  cout << "Found: " << out.isize() << endl;
+  out.Write(oString);
 
-  //==============================
-
-
-  nn = 0;
-  for (i=0; i<hi1.lsize(); i++) {
- 
-
-    DNAVector d = hi1[i];
-    DNAVector r = d;
-    r.ReverseComplement();
-
-    int nF = 0;
-    int nR = 0;
-
-    cout << "Test seq " << hi1.Name(i) << endl;
-    int last = -1;
-    for (j=0; j<=d.lsize()-k*num12; j++) {
-      DNAVector subF, subR;
-      subF.SetToSubOf(d, j, k*num12);
-      subR.SetToSubOf(r, j, k*num12);
-      svec<KmerAlignCoreRecordWithScore> matchesF_lo;
-      svec<KmerAlignCoreRecordWithScore> matchesR_lo;
-      svec<KmerAlignCoreRecordWithScore> matchesF_hi;
-      svec<KmerAlignCoreRecordWithScore> matchesR_hi;
-
-      low.GetMatches(matchesF_lo, subF);
-      low.GetMatches(matchesR_lo, subR);
-      
-      high.GetMatches(matchesF_hi, subF);
-      high.GetMatches(matchesR_hi, subR);
-
-      if (matchesF_lo.isize() + matchesR_lo.isize() == 0 &&
-	  matchesF_hi.isize() + matchesR_hi.isize() == 3) {
-	//cout << "Found high exclusive!" << endl;
-	if (j == last+1) {
-
-	  DNAVector & dd = out_hi[out_hi.isize()-1];
-	  dd.resize(dd.isize()+1);
-	  dd[dd.isize()-1] = subF[subF.isize()-1];
-
-	} else {
-	  nn++;
-	  string name = ">kmer_" + Stringify(nn) + "_" + Stringify(out_lo.isize());
-	  out_hi.push_back(subF, name);
-	}
-  	last = j;
-	
-      }
-
-    }
-     
-  } 
-
-  cout << "Found: " << out_hi.isize() << endl;
-
-  
-  out_hi.Write("high_exclusive.fasta");
-  return 0;
+   return 0;
 
 }
