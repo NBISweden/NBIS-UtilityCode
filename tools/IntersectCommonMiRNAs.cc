005704110e95aebf2c897bae23aaf7cc1f39bce4
Bengt Sennblad <bengt.sennblad@scilfelab.se>
2020-01-15 08:18:20 +0100

    fixed feature option in IntersectCommonMiRNAs

--- tools/IntersectCommonMiRNAs.cc
+++ tools/IntersectCommonMiRNAs.cc
@@ -22,7 +22,8 @@ public:
   string source;
   bool byName;
   string gene;
-  string annot;
+  map<string, string> annot;
+  string feature; 
   
   bool operator < (const Annot & a) const {
     if (byName) {
@@ -39,6 +40,7 @@ public:
   {
     // limit = min(limit, min(stop-start, m.stop-m.start));
     if(chr == m.chr && ori == m.ori &&     // same contig and strand
+       feature == m.feature &&             // same feature!
        start <= m.stop && stop > m.start &&  // require overlap
        std::abs(start - m.start) < limit && std::abs(stop - m.stop) < limit)
       {
@@ -49,15 +51,38 @@ public:
     return(false);
   };
   
-  string print(const string& prefix, unsigned index) const {
+  string print(const string& prefix, unsigned index, const bool& keepAnnot) const {
     ostringstream oss;
-    oss << chr << "\tmapped\tmiRNA\t"
+    oss << chr << "\tmapped\t"
+	<< feature << "\t"
 	<< start << "\t"
 	<< stop << "\t.\t"
 	<< ori << "\t.\t"
 	<< "gene_id \"" << prefix << "MI" << index << "\"; "
-	<< "transcript_id \"" << prefix << "TI" << index << "\"; "
-	<< "gene_name \"" << prefix << "MI" << index << "\"; ";
+	<< "transcript_id \"" << prefix << "TI" << index << "\"; ";
+    if(keepAnnot)
+      {
+	oss  << "gene_name \"" << annot.find("gene_name")->second << "\"";
+      // 	cerr << "\n\n" << name << endl;
+      // 	StringParser s;
+      // 	s.SetLine(annot, ";");
+      // 	for (unsigned i=0; i<s.GetItemCount(); i++)
+      // 	  {
+      // 	    cerr << s.AsString(i) << endl;
+      // 	    StringParser t;
+      // 	    t.SetLine(s.AsString(i), " ");
+      // 	    if(t.AsString(0) != "gene_id" &&
+      // 	       t.AsString(0) != "transcript_id")
+      // 	      {
+      // 	    cerr << s.AsString(i) << endl;
+      // 		oss << s.AsString(i) << "; ";
+      // 	      }
+      // 	  }
+      }
+    else
+      {
+	oss << "gene_name \"" << prefix << "MI" << index << "\"; ";
+      }
     return(oss.str());
   };
 };
@@ -80,13 +105,16 @@ void Load(svec<Annot> & annot, const string fileName,
       continue;
     Annot a;
     a.chr = parser.AsString(0);
+    a.feature = parser.AsString(2);
     a.start = parser.AsInt(3);
     a.stop = parser.AsInt(4);
     a.source = fileName;
     a.ori = parser.AsString(6);
     a.name = parser.AsString(9);
-    a.annot = parser.AsString(10);
-
+    for(int i = 10; i < parser.GetItemCount(); i += 2)
+      {
+	a.annot[parser.AsString(i)] = parser.AsString(i+1);
+      }
     // StringParser nn;
     // if (parser.GetItemCount() > 13) {
     //   nn.SetLine(parser.AsString(13), "Name=");
@@ -107,6 +135,7 @@ int main( int argc, char** argv )
   commandArg<int> diffCmmd("-d","allowed diff limit for start and stop positions when merging adjacent miRNAs", 25);
   commandArg<string> prefixCmmd("-p","gene name prefix", "miRNA");
   commandArg<string> feStringCmmd("-F", "Only include these features in out ut gtf (comma-separated list)","");
+  commandArg<bool> keepAnnotCmmd("-a", "Keep annotations (except gene_id and transcript_id)", false);
   commandLineParser P(argc,argv);
   P.SetDescription("Intersects two GTF files.");
   P.registerArg(qCmmd);
@@ -114,6 +143,7 @@ int main( int argc, char** argv )
   P.registerArg(diffCmmd);
   P.registerArg(prefixCmmd);
   P.registerArg(feStringCmmd);
+  P.registerArg(keepAnnotCmmd);
  
   P.parse();
   
@@ -121,6 +151,7 @@ int main( int argc, char** argv )
   string filter = P.GetStringValueFor(fCmmd);
   int diffLimit = P.GetIntValueFor(diffCmmd);
   string prefix = P.GetStringValueFor(prefixCmmd);
+  bool keepAnnot = P.GetBoolValueFor(keepAnnotCmmd);
 
   set<string> features;
   string featurestring  = P.GetStringValueFor(feStringCmmd);
@@ -143,7 +174,7 @@ int main( int argc, char** argv )
 	  j++;
 	}
       while(j != input.end() && i->merge(*j, diffLimit));
-      cout << i->print(prefix, index++) <<endl;
+      cout << i->print(prefix, index++, keepAnnot) <<endl;
       i = j;
     }
 
